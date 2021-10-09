1258079077c4e09aef71f6a6e086fc99fcc4371c
Mustapha Ghannouchi <mustapha.ghannouchi@esprit.tn>
2021-10-09 22:48:33 +0100

    #6 CRUD created

--- src/api/todo/TodoDataService.js
+++ src/api/todo/TodoDataService.js
@@ -12,5 +12,23 @@ class TodoDataService {
         return axios.delete(`http://localhost:8080/users/${name}/todos/${id}`)
         
     }
+
+    retrieveTodo(name,id){
+        return axios.get(`http://localhost:8080/users/${name}/todos/${id}`)
+    }
+
+     updateTodo(name,id, todo){
+
+        return axios.put(`http://localhost:8080/users/${name}/todos/${id}`, todo)
+        
+    }
+
+    createTodo(name, todo){
+
+        return axios.post(`http://localhost:8080/users/${name}/todos/`, todo)
+        
+    }
+
+
 }
 export default new TodoDataService()
